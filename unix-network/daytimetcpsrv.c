caf917a59310efd38e60014c180cf8388e045186
Imgaojp <imgaojp@gmail.com>
2016-11-30 15:27:16 +0800

    daytimeser

---
+++ unix-network/daytimetcpsrv.c
@@ -0,0 +1,26 @@
+#include"unp.h"
+#inlcude<time.h>
+int int main(int argc, char const **argv)
+{
+	/* code */
+	int listenfd,connfd;
+	struct sockaddr_in servaddr;
+    char buff[MAXLINE];
+    time_t ticks;
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(14);
+    Bind(listenfd, (SA *) NULL, NULL);
+    Listen(listenfd,LISTENQ);
+    for (;   ; ) {
+        connfd = Accept(listenfd, (SA *) NULL, NULL);
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        Write(connfd, buff, strlen(buff));
+
+        Close(connfd);
+    }
+	return 0;
+}
